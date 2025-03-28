9411d5d51a0ae4b9bb3a4884ce611a666bb6f2fe
JakubDolecki <jakubdolecki1@wp.pl>
2025-03-28 12:23:28 +0100

    zad 1 tablice

--- Zadanie 8/Program.cs
+++ Zadanie 8/Program.cs
@@ -10,6 +10,21 @@ namespace Zadanie_8
     {
         static void Main(string[] args)
         {
+            int[] tab = new int[1000];
+            int i;
+            int n;
+            Console.WriteLine("podaj ile elementów:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Podaj element {0}:", i);
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", tab[i]);
+            }
+            Console.ReadKey(true);
         }
     }
 }
